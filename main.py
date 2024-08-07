760fa837ad61f190c599810acc1875e40c42ca0b
Roshan Studley <OnyxAbove@users.noreply.github.com>
2024-08-07 13:14:42 -0700

    use template instead of generating pdf from scratch

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from pypdf import PdfReader, PdfWriter
+from fillpdf import fillpdfs
 
-def create_pdf(filename):
-    writer = PdfWriter()
-    writer.add_blank_page(215.9, 279.4)
+def main():
+    form_fields = list(fillpdfs.get_form_fields('form.pdf').keys())
 
-    with open(filename, 'wb') as file:
-        writer.write(file)
+    name = input("Enter the recipient's name: ")
+    reason = input("Enter the reason for the Thank You card: ")
+
+    data_dict = {
+        form_fields[0]: str(name),
+        form_fields[1]: str(reason),
+}
+
+    fillpdfs.write_fillable_pdf('form.pdf', 'ty.pdf', data_dict, True)
+    fillpdfs.flatten_pdf('ty.pdf', 'ty.pdf', as_images=False)
 
 if __name__ == '__main__':
-    create_pdf('ty.pdf')
+    main()
